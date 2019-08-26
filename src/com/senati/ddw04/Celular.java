f4ca7ea6bc027f5946409e52539676d909e4ea46
Instructor <Instructor@DESKTOP-CNLIIN1>
2019-08-26 08:54:41 -0500

    Commit 1 : Registrando por primera vez nuestro repositorio

---
+++ src/com/senati/ddw04/Celular.java
@@ -0,0 +1,45 @@
+package com.senati.ddw04;
+
+public class Celular {
+	//Atributos de la clase
+	int id;
+	String modelo;
+	float precio;
+	//Constructor se emplea al momento de crear el objeto
+	//Este constructor recibe automaticamente los parametros de inicio
+	public Celular(int id, String modelo, float precio) {
+		super();
+		this.id = id;
+		this.modelo = modelo;
+		this.precio = precio;
+	}
+	//Constructor que no recibe parametros
+	public Celular() {
+		super();
+	}
+	//Getters y Setters
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getModelo() {
+		return modelo;
+	}
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	public float getPrecio() {
+		return precio;
+	}
+	public void setPrecio(float precio) {
+		this.precio = precio;
+	}
+	@Override
+	public String toString() {
+		return "Celular [id=" + id + ", modelo=" + modelo + ", precio=" + precio + "]";
+	}
+	
+	
+}
